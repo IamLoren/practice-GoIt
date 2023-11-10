2b815465a4c5ea37534150be5e1988f8e88717b1
Nikolay Mosalov <73695807+Rom-dev-tech@users.noreply.github.com>
2023-11-10 17:18:00 +0200

    update

--- Q&A-8-9module/js/refs.js
+++ Q&A-8-9module/js/refs.js
@@ -1,8 +1,19 @@
-const counterControlsPanelEl = document.querySelector('.js-counter-controls')
-const storagePanelEl = document.querySelector('.js-save')
-const displayEl = document.querySelector('.js-counter-value')
+const counterControlsPanelEl = document.querySelector('.js-counter-controls');
+const storagePanelEl = document.querySelector('.js-save');
+const displayEl = document.querySelector('.js-counter-value');
 
 const bodyEl = document.body;
 const checkBoxEl = document.querySelector('#theme-switch');
 
-export { counterControlsPanelEl, storagePanelEl, displayEl, bodyEl, checkBoxEl };
+const formEl = document.querySelector('form');
+const ulEl = document.querySelector('.js-todos-list');
+
+export {
+  counterControlsPanelEl,
+  storagePanelEl,
+  displayEl,
+  bodyEl,
+  checkBoxEl,
+  formEl,
+  ulEl,
+};
---
+++ Q&A-8-9module/js/todo_list.js
@@ -0,0 +1,72 @@
+// - Викоритовуй шаблон списку завдань з файлу[todoList.html](./todolist.html) для створення функціоналу для списку завдань.
+//     - Використовуй поширення подій а також local storage, щоб при оновленні твої завдання залишалися на сторінці.Для цього використовуй допоміжну функцію setTasksOnLoad, яка буде отримувати далі з LS і додаватиме їх в html
+// - При виклику функції addTask зберігай завдання local storage і додавай його у розмітку
+// - При deleteTask знаходь завдання по тексту, та видаляй його і з local storage і з розмітки.
+
+import { formEl, ulEl } from './refs.js';
+import localStorageApi from './storage.js';
+
+let items = [];
+const TASK_KEY = 'ToDos';
+
+// Function to create new task element, return LI element
+const createTodos = ({ id, text }) => {
+  const markup = `
+        <li>
+          <input disabled value="${text}"/>
+          <div>
+            <button data-id="${id}">Delete</button>
+          </div>
+        </li>
+        `;
+
+  return markup;
+};
+
+// Function to handle submit action, call addTask inside
+function onSubmit(event) {
+  event.preventDefault();
+
+  const taskQuery = event.currentTarget.elements.todos.value.trim();
+
+  if (!taskQuery) {
+    return alert('Task is empty!');
+  }
+
+  const toDoTask = {
+    id: Date.now(),
+    text: taskQuery,
+  };
+
+  addTask(toDoTask);
+
+  event.target.reset();
+}
+
+// Function to add a task to the list (use createNewTaskElement) and add to LS
+function addTask(value) {
+  const markUp = createTodos(value);
+  items.push(value);
+  ulEl.insertAdjacentHTML('afterbegin', markUp);
+  localStorageApi.save(TASK_KEY, items);
+}
+
+// Function to delete a task from the list and delete from LS
+function deleteTask() {}
+
+// Function to get tasks from LS and set them into ul
+function setTasksOnLoad() {
+  const lsToDos = localStorageApi.load(TASK_KEY);
+  if (lsToDos) {
+    items = [...lsToDos];
+    const template = items
+      .map((el) => createTodos(el))
+      .reverse()
+      .join('');
+    ulEl.innerHTML = template;
+  }
+}
+
+formEl.addEventListener('submit', onSubmit);
+ulEl.addEventListener('click', deleteTask);
+document.addEventListener('DOMContentLoaded', setTasksOnLoad);
