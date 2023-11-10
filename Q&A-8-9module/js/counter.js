105db7c155366fce7e6efa689b82c24098af6be6
Ariy83 <143614766+Ariy83@users.noreply.github.com>
2023-11-10 15:27:16 +0200

    +

--- Q&A-8-9module/js/counter.js
+++ Q&A-8-9module/js/counter.js
@@ -1,27 +1,57 @@
-import { counterControlsPanelEl, storagePanelEl, displayEl } from "./refs.js"
+import { counterControlsPanelEl, storagePanelEl, displayEl } from "./refs.js";
+import lokalStorageAPI from "./storage.js";
 
-let currentCounter = 0
+let currentCounter = Number(displayEl.textContent);
+const LOCAL_KEY = "LOCAL_COUNTER";
 
-counterControlsPanelEl.addEventListener('click', onCounterControlsPanelClick)
-storagePanelEl.addEventListener('click', onStoragePanelClick)
+counterControlsPanelEl.addEventListener("click", onCounterControlsPanelClick);
+storagePanelEl.addEventListener("click", onStoragePanelClick);
+document.addEventListener("DOMContentLoaded", renderPage);
 
 function onCounterControlsPanelClick({ target }) {
-    if (target.nodeName !== 'BUTTON') {
-        return
-    }
-    const clickedBtn = target.dataset.action
-    switch (clickedBtn) {
-        case "increase": currentCounter++
-            break
-        case "decrease": currentCounter--
-            break
-        case "reset": currentCounter = 0
-            break
-        default :
-            alert("Unknown value!")
-    }
-    displayEl.textContent = currentCounter
+  if (target.nodeName !== "BUTTON") {
+    return;
+  }
+
+  const clickedBtn = target.dataset.action;
+
+  switch (clickedBtn) {
+    case "increase":
+      currentCounter++;
+      break;
+    case "decrease":
+      currentCounter--;
+      break;
+    case "reset":
+      currentCounter = 0;
+      break;
+    default:
+      alert("Unknown value!");
+  }
+
+  displayEl.textContent = currentCounter;
 }
 
-function onStoragePanelClick({ target }) {}
+function onStoragePanelClick({ target }) {
+  if (target.nodeName !== "BUTTON") {
+    return;
+  }
 
+  const clickedBtn = target.dataset.action;
+
+  if (clickedBtn === "save") {
+    lokalStorageAPI.save(LOCAL_KEY, currentCounter);
+    return;
+  }
+
+  lokalStorageAPI.remove(LOCAL_KEY);
+}
+
+function renderPage() {
+  const lsData = lokalStorageAPI.load(LOCAL_KEY);
+
+  if (lsData) {
+    currentCounter = Number(lsData);
+    displayEl.textContent = currentCounter;
+  }
+}
