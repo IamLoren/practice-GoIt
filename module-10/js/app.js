ab67f3af396e303ad32d3db036fe2b50ccf688cb
Olegopolonets <54370738+Olegopolonets@users.noreply.github.com>
2023-11-13 17:04:46 +0200

    delete

--- module-10/js/app.js
+++ module-10/js/app.js
@@ -1,16 +1,25 @@
-import { getStudents, getStudentById, addStudent, updateStudent } from "./students-api.js";
-
+import {
+  getStudents,
+  getStudentById,
+  addStudent,
+  updateStudent,
+  deleteStudent,
+} from "./students-api.js";
 
 // getStudents().then(console.log).catch(console.log);
 // getStudentById(5).then(console.log).catch(console.log);
 
 const student = {
-    firstName: 'Sister',
-    lastName: 'Miller',
-    country: 'Ukraine',
-    age: 35,
-    city: 'Lviv',
+  firstName: "Sister",
+  lastName: "Miller",
+  country: "Ukraine",
+  age: 35,
+  city: "Lviv",
 };
 // addStudent(student).then(console.log).catch(console.log);
-updateStudent(5,{firstName: 'Igor', lastName: 'Olimpiskich'}).then(console.log).catch(console.log);
-getStudents().then(console.log).catch(console.log);
+// updateStudent(5, { firstName: "Igor", lastName: "Olimpiskich" })
+//   .then(console.log)
+//   .catch(console.log);
+// getStudents().then(console.log).catch(console.log);
+// deleteStudent(3).then(console.log).catch(console.log);
+// getStudents().then(console.log).catch(console.log);
