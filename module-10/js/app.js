95fb2c731580964390b4433047eab581c3e09e6f
Serhii-Kuzmin <seryy8248@gmail.com>
2023-11-14 13:51:49 +0100

    commit-task-01

--- module-10/js/app.js
+++ module-10/js/app.js
@@ -1,25 +1,84 @@
 import {
   getStudents,
-  getStudentById,
   addStudent,
-  updateStudent,
   deleteStudent,
 } from "./students-api.js";
 
-// getStudents().then(console.log).catch(console.log);
-// getStudentById(5).then(console.log).catch(console.log);
+import { listEl, openModalBtnEl } from "./refs.js";
+import { modal } from "./modal.js"; 
+
+const items = [];
+
+openModalBtnEl.addEventListener('click',onOpenModalBtnClick)
+
+function onOpenModalBtnClick() {
+  modal.show();
+
+  document.querySelector('form').addEventListener('submit', onSubmit)
+
+}
+
+function onSubmit(event) {
+event.preventDefault()
+
+const {avatar, firstName, lastName, age, country, city} = event.currentTarget.elements;
 
 const student = {
-  firstName: "Sister",
-  lastName: "Miller",
-  country: "Ukraine",
-  age: 35,
-  city: "Lviv",
+  firstName: firstName.value,
+  lastName: lastName.value,
+  age: age.value,
+  country: country.value,
+  city: city.value,
+  avatar: avatar.value,
+}
+
+addStudent(student)
+.then(res => {
+  items.push(res)
+
+  return createStudentCard(student)
+
+})
+.then(res => {
+  listEl.insertAdjacentHTML('beforeend', res)
+})
+.catch(err => {
+  console.log(err);
+})
+
+event.currentTarget.reset();
+modal.close();
+}
+
+function createStudentCard(student) {
+  const {avatar, firstName, lastName, age, country, city, id} = student; 
+
+  return `
+  <li class="photo-card">
+        <img
+          src="${avatar}"
+          alt="${firstName}"
+        />
+        <button data-id=${id} type="button" class="delete-btn js-delete-btn">&#9988;</button>
+
+        <div class="wrapper">
+          <div class="info" >
+          <b class="student-info">${firstName}</b>
+          <b class="student-info">${lastName}</b>
+          <b class="student-info">${age}</b>
+          </div>
+
+          <div class="info">
+          <b class="student-info">${country}</b>
+          <b class="student-info">${city}</b>
+          </div>
+        </div>
+      </li>
+  `;
 };
-// addStudent(student).then(console.log).catch(console.log);
-// updateStudent(5, { firstName: "Igor", lastName: "Olimpiskich" })
-//   .then(console.log)
-//   .catch(console.log);
-// getStudents().then(console.log).catch(console.log);
-// deleteStudent(3).then(console.log).catch(console.log);
-// getStudents().then(console.log).catch(console.log);
+
+function renderMarkup(data) {
+  const markup = data.map(student => createStudentCard(student)).join('')
+
+  listEl.innerHTML = markup;
+}
---
+++ module-10/js/refs.js
@@ -0,0 +1,2 @@
+export const openModalBtnEl = document.querySelector('.js-modal-btn');
+export const listEl = document.querySelector('.gallery');
