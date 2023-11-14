47a66d25c03c4a0d4cb02477ff8c11e0931b468c
Mary <32392607+MartFrida@users.noreply.github.com>
2023-11-14 14:09:57 +0100

    added ava

--- module-10/js/app.js
+++ module-10/js/app.js
@@ -5,11 +5,13 @@ import {
 } from "./students-api.js";
 
 import { listEl, openModalBtnEl } from "./refs.js";
-import { modal } from "./modal.js"; 
+import { modal } from "./modal.js";
 
-const items = [];
+let items = [];
+const defaultAvatar='https://mir-s3-cdn-cf.behance.net/project_modules/1400/10f13510774061.560eadfde5b61.png';
 
-openModalBtnEl.addEventListener('click',onOpenModalBtnClick)
+openModalBtnEl.addEventListener('click', onOpenModalBtnClick);
+document.addEventListener('DOMContentLoaded', onRenderPage);
 
 function onOpenModalBtnClick() {
   modal.show();
@@ -19,39 +21,39 @@ function onOpenModalBtnClick() {
 }
 
 function onSubmit(event) {
-event.preventDefault()
+  event.preventDefault()
 
-const {avatar, firstName, lastName, age, country, city} = event.currentTarget.elements;
+  const { avatar, firstName, lastName, age, country, city } = event.currentTarget.elements;
 
-const student = {
-  firstName: firstName.value,
-  lastName: lastName.value,
-  age: age.value,
-  country: country.value,
-  city: city.value,
-  avatar: avatar.value,
-}
+  const student = {
+    firstName: firstName.value,
+    lastName: lastName.value,
+    age: age.value,
+    country: country.value,
+    city: city.value,
+    avatar: avatar.value || defaultAvatar,
+  }
 
-addStudent(student)
-.then(res => {
-  items.push(res)
+  addStudent(student)
+    .then(res => {
+      items.push(res)
 
-  return createStudentCard(student)
+      return createStudentCard(student)
 
-})
-.then(res => {
-  listEl.insertAdjacentHTML('beforeend', res)
-})
-.catch(err => {
-  console.log(err);
-})
+    })
+    .then(res => {
+      listEl.insertAdjacentHTML('beforeend', res)
+    })
+    .catch(err => {
+      console.log(err);
+    })
 
-event.currentTarget.reset();
-modal.close();
+  event.currentTarget.reset();
+  modal.close();
 }
 
 function createStudentCard(student) {
-  const {avatar, firstName, lastName, age, country, city, id} = student; 
+  const { avatar, firstName, lastName, age, country, city, id } = student;
 
   return `
   <li class="photo-card">
@@ -81,4 +83,14 @@ function renderMarkup(data) {
   const markup = data.map(student => createStudentCard(student)).join('')
 
   listEl.innerHTML = markup;
+}
+
+function onRenderPage() {
+  getStudents()
+    .then(res => {
+      items = [...res];
+      console.log(items);
+      renderMarkup(items)
+    })
+    .catch(console.log)
 }
