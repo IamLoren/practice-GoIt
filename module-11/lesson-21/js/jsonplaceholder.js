bd89d255c3ad1784a52f5525cb0869146a73a18d
Nikolay Mosalov <73695807+Rom-dev-tech@users.noreply.github.com>
2023-11-15 13:00:01 +0200

    added module 11

---
+++ module-11/lesson-21/js/jsonplaceholder.js
@@ -0,0 +1,21 @@
+const BASE_URL = 'https://jsonplaceholder.typicode.com';
+
+// ! fetch
+// export function getUsers() {
+//   const END_POINT = 'users';
+//   const searchParams = new URLSearchParams({
+//     _limit: 5,
+//     _page: 2,
+//   });
+
+//   return fetch(`${BASE_URL}/${END_POINT}?${searchParams}`).then((response) => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     return response.json();
+//   });
+// }
+
+// ! axios
+
+// ! pagination
