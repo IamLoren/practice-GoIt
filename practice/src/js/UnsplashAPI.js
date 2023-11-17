8e51f0879156b03c7ba2e28862c006c79918a689
Nikolay Mosalov <73695807+Rom-dev-tech@users.noreply.github.com>
2023-11-17 13:59:46 +0200

    added practice

---
+++ practice/src/js/UnsplashAPI.js
@@ -0,0 +1,45 @@
+// https://unsplash.com/documentation#search-photos - API
+// https://www.npmjs.com/package/tui-pagination - Бібліотека "tui-pagination"
+
+import axios from 'axios';
+
+export class UnsplashAPI {
+  // Приватні властивості
+  #BASE_URL = 'https://api.unsplash.com';
+  #API_KEY = 'LxvKVGJqiSe6NcEVZOaLXC-f2JIIWZaq_o0WrF8mwJc';
+  #query = '';
+
+  // constructor() {
+  //   this.page = 1;
+  //   this.query = null;
+  // }
+
+  // Запит для відображення картинок, при загрузці сторінки
+  getPopularPhotos(page) {
+    return axios.get(`${this.#BASE_URL}/search/photos`, {
+      params: {
+        query: 'random',
+        page,
+        per_page: 12,
+        client_id: this.#API_KEY,
+      },
+    });
+  }
+
+  // Запит картинок по ключовому слову
+  fetchPhotosByQuery(page) {
+    return axios.get(`${this.#BASE_URL}/search/photos`, {
+      params: {
+        query: this.#query,
+        page,
+        per_page: 12,
+        client_id: this.#API_KEY,
+      },
+    });
+  }
+
+  // Сетер для запису searchQuery
+  set query(newQuery) {
+    this.#query = newQuery;
+  }
+}
